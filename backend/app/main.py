f2e8f4964e9d6d6755b2af529f61a89999b50297
David <davidrojasmorales21@gmail.com>
2026-05-22 21:49:42 -0500

    fix: agregar origen exacto de vercel para solucionar CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,16 +9,21 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ShiftCash API")
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://shift-cash-spk1ah2yg-davidrojasmorales21-8377s-projects.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:5173", "https://*.vercel.app"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(users.router)
-app.include_router(transactions.router)
 app.include_router(transactions.router)
 app.include_router(categories.router)
 app.include_router(budgets.router)
